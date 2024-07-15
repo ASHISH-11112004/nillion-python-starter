ae9320447c1c19357cfe1da4daae84bc43c0ef8a
ASHISH-11112004 <ashish2004bkp@gmail.com>
2024-07-15 16:41:41 +0000

    my new nada program

--- quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
+++ quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
@@ -1,12 +1,26 @@
 from nada_dsl import *
+
 def nada_main():
 
+    # Define the parties involved
     party1 = Party(name="Party1")
+    party2 = Party(name="Party2")
 
+    # Input secret integers for Party1
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    new_int = my_int1 + my_int2
+    # Input secret integers for Party2
+    my_int3 = SecretInteger(Input(name="my_int3", party=party2))
 
-    return [Output(new_int, "my_output", party1)]
+    # Perform operations
+    sum_int = my_int1 + my_int2
+    product_int = sum_int * my_int3
+    final_int = product_int - my_int2
+
+    # Return the outputs
+    return [
+        Output(sum_int, "sum_output", party1),
+        Output(product_int, "product_output", party2),
+        Output(final_int, "final_output", party1)
+    ]
